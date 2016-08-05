7d84972133339124c4bb04dac1da9f5793f126b6
Douglas Goodwin <dgoodwin@gmail.com>
2016-08-05 12:45:40 -0700

    tidy

--- part_2/fetch_parse_emit.py
+++ part_2/fetch_parse_emit.py
@@ -8,13 +8,14 @@ import xmltodict
 import requests
 # from tzlocal import get_localzone -- use this if you're not sure about the local TZ
 from jinja2 import Template, Environment, FileSystemLoader
+# import rethinkdb as rdb
 
 # set up all the variables
 DEBUG = False
 XML_URL = 'http://meetingplannerxml'
 xmlfile = 'MeetingPlanner.xml'
 timeout = 2 # seconds
-
+# time
 local_tz = pytz.timezone('America/Los_Angeles') # use your local timezone name here
 UTC_tz = pytz.timezone('UTC')
 thenow = datetime.now(local_tz)
@@ -22,13 +23,17 @@ thenow_utc = datetime.now(UTC_tz)
 # offset-naive and offset-aware datetimes ?
 # remove the timezone awareness, yuk!
 naive_utc = thenow_utc.replace(tzinfo=None)
-
-# TypeError: can't compare offset-naive and offset-aware datetimes
-# thenow = datetime.now()
 todaydisplay = thenow.strftime("%B %d, %Y")
 timefmt = "%m/%d/%Y %I:%M:%S %p"
 signurl = "http://127.0.0.1:8888/api"
 # signurl = "http://signage.metro.net/api"
+
+# Connect to the RethinkDB server
+# RDB_HOST =  os.environ.get('RDB_HOST') or 'localhost'
+# RDB_PORT = os.environ.get('RDB_PORT') or 28015
+# RDB_DB = 'meetings'
+#
+# rdb_conn = rdb.connect(host=RDB_HOST, port=RDB_PORT, db=RDB_DB)
 
 def parse_mptime(timestr="8/8/2016 1:00:00 PM", timefmt = "%m/%d/%Y %I:%M:%S %p"):
     # MP time is UTC
@@ -46,6 +51,7 @@ def parse_mptime(timestr="8/8/2016 1:00:00 PM", timefmt = "%m/%d/%Y %I:%M:%S %p"
     timedict["local"] = localtime
     timedict["displaytime"] = timedict['local'].strftime('%I:%M %p')
     timedict["displaydate"] = timedict['local'].strftime('%m/%d/%Y')
+    timedict["json_date"] = localtime.isoformat()
     # is this time in the future?
     utcnow = utc_tz.localize(datetime.utcnow())
     timedict["isfuture"] = (utcnow <= utc_time)
@@ -53,7 +59,6 @@ def parse_mptime(timestr="8/8/2016 1:00:00 PM", timefmt = "%m/%d/%Y %I:%M:%S %p"
     localnow = local_tz.localize(datetime.now())
     timedict["endstoday"] = (localnow.year,localnow.month,localnow.day) == (localtime.year,localtime.month,localtime.day)
     return timedict
-
 
 def modification_date(afile):
     t = os.path.getmtime(afile)
@@ -82,6 +87,7 @@ def tidymeup(s):
     s = s.replace("\'", '')
     return s
 
+# def gimme_json(xmlfile,todaydisplay,lastupdate,rdb):
 def gimme_json(xmlfile,todaydisplay,lastupdate):
     updatestr = lastupdate.strftime("%B %d, %Y %I:%M %p")
     with open(xmlfile) as fd:
@@ -91,23 +97,24 @@ def gimme_json(xmlfile,todaydisplay,lastupdate):
     reslist = []
 
     # get rid of duplicates
-    for i,r in enumerate(reservations_dict):
-        if r not in reslist:
-            reslist.append(r)
+    for i,res in enumerate(reservations_dict):
+        if res not in reslist:
+            reslist.append(res)
 
     # sort and add order id
     sorted(reslist, key=lambda k: k['room_res_start_dt'])
-    for i,r in enumerate(reslist):
-        r['order']=i
+    for i,res in enumerate(reslist):
+        res['order']=i
 
     # top off the list with empty data
     empties = ( 16-len(reslist) )
     for i,e in enumerate( range(empties),100 ):
-        r=OrderedDict([(u'order', i), (u'user_id', None), (u'res_id', u''), (u'room_id', u''), (u'Expr1', None), (u'Expr2', u''), (u'Expr3', None), (u'room_res_start_dt', u''), (u'room_res_end_dt', u''), (u'ts', u''), (u'displaytime', u''),(u'room_name', u''), (u'res_general_desc', u''), (u'res_activity_cd', None), (u'code_table_item_cd', None)])
+        r=OrderedDict([(u'order', i), (u'id', u''), (u'user_id', None), (u'res_id', u''), (u'room_id', u''), (u'Expr1', None), (u'Expr2', u''), (u'Expr3', None), (u'room_res_start_dt', u''), (u'room_res_end_dt', u''), (u'ts', u''), (u'displaytime', u''),(u'room_name', u''), (u'res_general_desc', u''), (u'res_activity_cd', None), (u'code_table_item_cd', None)])
         reslist.append(r)
 
     respackage = {'todaydisplay':todaydisplay,'current':[],'inprocess':[],'future':[],'lastupdate':updatestr}
     for er in reslist:
+        er['id'] = er['res_id']
         st_dict = parse_mptime(er['room_res_start_dt'])
         et_dict = parse_mptime(er['room_res_end_dt'])
         if (et_dict['isfuture']):
@@ -116,9 +123,10 @@ def gimme_json(xmlfile,todaydisplay,lastupdate):
                 er['ts'] = st_dict['timestamp']
                 if et_dict['endstoday']:
                     respackage['current'].append(er)
-                    print(er)
+                    # add it to rethinkdb
+                    # rdb.db("meetings").table("current").insert(er).run(rdb_conn)
             except:
-                # maybe no date was set?
+            #     # maybe no date was set?
                 pass
 
             respackage['inprocess'].append(er)
@@ -136,6 +144,7 @@ while True:
         xmlfile,lastupdate = fetchfile(XML_URL,timeout,xmlfile)
         # DEBUG = True
 
+    # goj = gimme_json(xmlfile,todaydisplay,lastupdate,rdb)
     goj = gimme_json(xmlfile,todaydisplay,lastupdate)
     # print(goj)
     r = requests.post( signurl, json=goj )
